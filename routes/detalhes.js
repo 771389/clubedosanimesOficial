9c28feb068a1094aade456af603f1fc6896a3445
unknown <flavinhaalmeiiida234@gmail.com>
2024-09-16 23:08:02 -0300

    a

--- routes/detalhes.js
+++ routes/detalhes.js
@@ -3,14 +3,11 @@ const router = express.Router();
 const axios = require('axios');
 
 const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
-
-// Defina a base URL da imagem
 const baseImageUrl = 'https://cdn.appanimeplus.tk/img/';
 
-router.get('/detalhes/:id', (req, res) => {
-  const itemId = req.params.id;
+router.get('/detalhes/:id', async (req, res) => {
+  const itemId = parseInt(req.params.id, 10);
 
-  // Validação básica do itemId
   if (!itemId || isNaN(itemId)) {
     return res.status(400).send('Parâmetro id inválido');
   }
@@ -21,23 +18,39 @@ router.get('/detalhes/:id', (req, res) => {
     headers: {
       'User-Agent': userAgent,
     },
-    timeout: 5000  // Timeout de 5 segundos
+    timeout: 5000
   };
 
-  axios.get(apiUrl, axiosConfig)
-    .then((response) => {
-      if (response.status === 200) {
-        const data = response.data;
-        res.send(data);
-      } else {
-        console.log(`A solicitação falhou com o código de status: ${response.status}`);
-        res.status(response.status).send(`Erro na solicitação: ${response.status}`);
-      }
-    })
-    .catch((error) => {
+  try {
+    const response = await axios.get(apiUrl, axiosConfig);
+
+    if (response.status === 200) {
+      const data = response.data;
+
+      // Adicionando a URL base a cada category_icon
+      data.forEach(item => {
+        if (item.category_icon) {
+          item.category_icon = baseImageUrl + item.category_icon;
+        }
+      });
+
+      res.send(data);
+    } else {
+      console.log(`A solicitação falhou com o código de status: ${response.status}`);
+      res.status(response.status).send(`Erro na solicitação: ${response.status}`);
+    }
+  } catch (error) {
+    if (error.code === 'ECONNABORTED') {
+      console.error('A solicitação excedeu o tempo limite.');
+      res.status(504).send('A solicitação excedeu o tempo limite.');
+    } else if (error.response) {
+      console.error(`Erro na resposta da API: ${error.response.status}`);
+      res.status(error.response.status).send(`Erro na resposta da API: ${error.response.status}`);
+    } else {
       console.error('Ocorreu um erro na solicitação:', error.message || error);
       res.status(500).send('Erro interno do servidor');
-    });
+    }
+  }
 });
 
 module.exports = router;
