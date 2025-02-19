d79d64b3e4e763ecb4b53c128f1cf6fecef6af8d
Clube dos Mods <vitormanoel584@gmail.com>
2025-02-18 23:48:37 -0300

    teste00998

--- api/index.js
+++ api/index.js
@@ -37,11 +37,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/imagens', routesimg);
 
 // 🔹 Rotas protegidas (JWT necessário)
-app.use('/api/home', verifyToken, [
-  routesanmdub, routescategorias, routescategoria, 
-  routesfilte, routesanmleg, routeslancamentos, 
-  routespopulares
-]);
+app.use('/api/home/anime_dub', verifyToken, routesanmdub);
+app.use('/api/home/anime_leg', verifyToken, routesanmleg);
+app.use('/api/home/lancamentos', verifyToken, routeslancamentos);
+app.use('/api/home/populares', verifyToken, routespopulares);
+
 
 app.use('/api/filmes', verifyToken, [routesfilmedub, routesfilmeleg]);
 app.use('/api/anime', verifyToken, [routesdetalhes, routesepisodios, routeseps, routespesquisar]);
